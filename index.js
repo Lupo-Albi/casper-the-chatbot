3e4070a9f87949647813959872bf6de699c02f10
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-05-28 21:56:34 -0300

    Testing DialogFlow webhook call

--- index.js
+++ index.js
@@ -10,21 +10,35 @@ app.use(bodyParser.json());
 app.post('/webhook', (req, res) => {
 	let body = req.body;
 
-	// Checks this is an event from a page subscription
-	if (body.object === 'page') {
-		// Iterates over each entry - there may be multiple if batched
-		body.entry.forEach(function(entry) {
-			// Gets the message. entry.messagin is an array but will only ever contain one message, so we get index 0
-			let webhook_event = entry.messaging[0];
-			console.log(webhook_event);
-		});
+	let intentName = body.queryResult.intent.displayName;
 
-		// Returns a '200 OK' response to all request
-		res.status(200).send('EVENT_RECEIVED');
-	} else {
-		// Returns a '404 Not Found' if event is not from a page subscription
-		res.sendStatus(404);
+	if (intentName === 'teste') {
+		res.json({
+			fulfillmentMessages: [
+				{
+					text: {
+						text: [ 'Text response from webhook' ]
+					}
+				}
+			]
+		});
 	}
+
+	// // Checks this is an event from a page subscription
+	// if (body.object === 'page') {
+	// 	// Iterates over each entry - there may be multiple if batched
+	// 	body.entry.forEach(function(entry) {
+	// 		// Gets the message. entry.messagin is an array but will only ever contain one message, so we get index 0
+	// 		let webhook_event = entry.messaging[0];
+	// 		console.log(webhook_event);
+	// 	});
+
+	// 	// Returns a '200 OK' response to all request
+	// 	res.status(200).send('EVENT_RECEIVED');
+	// } else {
+	// 	// Returns a '404 Not Found' if event is not from a page subscription
+	// 	res.sendStatus(404);
+	// }
 });
 
 app.get('/webhook', (req, res) => {
