3e3f2398aa1eb403bfd940b0c04d1ff1f3d314de
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-06-07 18:07:47 -0300

    Changing variables declarations

--- utils/getNoticias.js
+++ utils/getNoticias.js
@@ -1,24 +1,6 @@
 const Noticia = require('../models/noticia');
 const catchAsync = require('./catchAsync');
 
-const elements = []; // We put each generic template object here to create our carousel
-
-// Message to be sent to DialogFlow inside the fulfillmentMessages
-const carouselPayload = [
-	{
-		payload: {
-			facebook: {
-				attachment: {
-					payload: {
-						elements // We put elements array here
-					}
-				}
-			}
-		},
-		platform: 'FACEBOOK'
-	}
-];
-
 // Messenger Quick Reply Template
 const nothingFound = [
 	{
@@ -35,6 +17,24 @@ const getNoticias = catchAsync(async (req, res, theme) => {
 	// console.log(query);
 	// Checks if the query returned any results
 	if (Array.isArray(query) && query.length) {
+		const elements = []; // We put each generic template object here to create our carousel
+
+		// Message to be sent to DialogFlow inside the fulfillmentMessages
+		const carouselPayload = [
+			{
+				payload: {
+					facebook: {
+						attachment: {
+							payload: {
+								elements // We put elements array here
+							}
+						}
+					}
+				},
+				platform: 'FACEBOOK'
+			}
+		];
+
 		query.map((card) => {
 			const { title, subtitle, image_url, url } = card;
 
@@ -64,21 +64,6 @@ const getNoticias = catchAsync(async (req, res, theme) => {
 		// Send this message if no result was found
 		res.send({ fulfillmentMessages: nothingFound });
 	}
-
-	carouselPayload = [
-		{
-			payload: {
-				facebook: {
-					attachment: {
-						payload: {
-							elements: [] // We put elements array here
-						}
-					}
-				}
-			},
-			platform: 'FACEBOOK'
-		}
-	];
 });
 
 module.exports = getNoticias;
